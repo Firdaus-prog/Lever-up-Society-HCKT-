58c22fb58bceb8b0d80cb6f78bb9c1d7a14a247a
Firdaus Aziz <mfirdausbaziz@gmail.com>
2021-07-16 21:11:19 +0100

    Main Character

--- Level-up/Assets/Script/Character/CharAnimation.cs
+++ Level-up/Assets/Script/Character/CharAnimation.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class CharAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject mainChar;
     void Start()
     {
-        
+        mainChar.GetComponent<Animation>().Play("Idle");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
